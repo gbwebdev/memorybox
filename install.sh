07af324efc70e2e003c3687ed73e5b4e8a3066bf
Guillaume BITON <guillaume@gbweb.fr>
2024-08-23 13:31:25 +0200

    Fixing install script

--- install.sh
+++ install.sh
@@ -14,16 +14,29 @@ function run_as_user {
 }
 
 if [ ! -d ./venv ]; then
+  echo "Creating python virtual environment..."
   run_as_user "python3 -m venv venv"
+  echo "...done."
 fi
 
+echo "Activating python virtual environment."
 run_as_user "source venv/bin/activate"
 
+echo "Installing dependencies..."
+apt update
 apt install -y libbluetooth-dev
 apt install -y libopenjp2-7 libtiff5-dev libtiff6
+echo "...done."
 
+echo "Creating instance directory..."
 mkdir -p /var/www/memorybox
 if [ ! -z $SUDO_UID ]; then
   chown $SUDO_UID /var/www/memorybox
-  run_as_user "source venv/bin/activate && pip install ./memorybox"
 fi
+echo "...done."
+
+echo "Installing memorybox python package..."
+run_as_user "source venv/bin/activate && pip install ./memorybox"
+echo "...done."
+
+echo "Installation complete !"
