2bdf1e9267bcd093afa9bf490e519824ba192da5
Guillaume BITON <guillaume@gbweb.fr>
2024-08-25 17:44:18 +0200

    Starting implementation of memories

--- memorybox/src/memorybox/fetch_memories.py
+++ memorybox/src/memorybox/fetch_memories.py
@@ -1,21 +1,41 @@
 import click
 import bs4
 import requests
-
 from pathlib import Path
+import shutil
+
 from flask import current_app
 
+from PIL import Image
+
 from memorybox.config import Config, MemoriesSourceType
-from memorybox import db
+from memorybox.model.memory import Memory
+from memorybox.db import db
+
 
 def _fetch_memories_from_local_directory():
     local_directory_path = Config().memories_local_path
     current_app.logger.info(f"Looking for packages in {local_directory_path}")
-    db.get_db()
-    for zip_file in Path(local_directory_path).rglob('*.zip'):
-        current_app.logger.info(f"Found zip file : {zip_file}")
-    for tar_file in Path(local_directory_path).rglob('*.tar'):
-        current_app.logger.info(f"Found tar file : {tar_file}")
+    for jpeg_file in Path(local_directory_path).rglob('*.[jJ][pP]*[gG]'):
+        filename = Path(jpeg_file).name
+        current_app.logger.info(f"Found JPEG file : {filename}")
+        res = Memory.query.filter_by(filename=filename).first()
+        if not res :
+            current_app.logger.info(f"File not found in database : {filename}")
+            new_path = Path(current_app.config['memories_path']).joinpath(filename)
+            shutil.move(jpeg_file, new_path)
+
+            thumbnail_path = Path(current_app.config['thumbnails_path']).joinpath(filename)
+            image = Image.open(new_path)
+            #Define the thumbnail size as a tuple (width, height)
+
+            base_width = 1280
+            wpercent = (base_width / float(image.size[0]))
+            hsize = int((float(image.size[1]) * float(wpercent)))
+            image = image.resize((base_width, hsize), Image.BOX)
+            image.save(thumbnail_path)
+            
+
 
 def _fetch_memories_from_remote_repository():
     current_app.logger.info("Fetching packages from remote repository.")
--- memorybox/src/memorybox/model/memory.py
+++ memorybox/src/memorybox/model/memory.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Integer, String, DateTime, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
+from datetime import datetime
 from memorybox.db import db
 
 class Memory(db.Model):
     """Model for a "memory" (a picture)"""
-    id: Mapped[Integer] = mapped_column(primary_key=True)
-    filename: Mapped[String] = mapped_column(unique=True)
-    author: Mapped[String]
-    release_date: Mapped[DateTime]
-    captation: Mapped[String]
-    printed: Mapped[Boolean]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    filename: Mapped[str] = mapped_column(unique=True)
+    author: Mapped[str]
+    release_date: Mapped[datetime]
+    captation: Mapped[str]
+    printed: Mapped[bool]
