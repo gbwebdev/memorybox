ab1270587bcc95de3bacec68f334d379783040db
Guillaume BITON <guillaume@gbweb.fr>
2024-08-25 22:18:28 +0200

    Improved init_db

--- memorybox/src/memorybox/tools/init_db.py
+++ memorybox/src/memorybox/tools/init_db.py
@@ -3,7 +3,9 @@ from flask import current_app
 from memorybox.db import db
 from memorybox.model import memory
 
-def init_db():
+def init_db(drop=False):
     with current_app.app_context():
         current_app.logger.debug(current_app.config)
+        if drop:
+            db.drop_all()
         db.create_all()
