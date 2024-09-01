f6d5495fe01cfa2ca91755d95f30cc2c628d7cde
Guillaume BITON <guillaume@gbweb.fr>
2024-09-01 11:56:51 +0200

    Moved print logic to js script file and added alert box

---
+++ memorybox/src/memorybox/static/js/print.js
@@ -0,0 +1,34 @@
+const wsEndpoint = location.protocol + '//' + location.host;
+
+var socket = io.connect(wsEndpoint);
+
+$('#printButton').on('click', function(event) {
+    console.log("Print button clicked !");
+    triggerPrint($(this).data('memory'));
+});
+
+// Listen for notifications from the server
+socket.on('notify_agent', function(data) {
+    console.log('Agent notified:', data.message);
+    // Here you can perform additional actions when the agent is notified
+});
+
+function triggerPrint(memoryId) {
+  socket.emit('print', {data: memoryId});
+  console.log("Requested print for memory id ", memoryId);
+  setPrintMessage("info", "Print request sent.");
+}
+
+function setPrintMessage(kind, message, duration=0) {
+  $('#printAlert').show();
+  $('#printAlert').removeClass();
+  $('#printAlert').addClass('alert');
+  $('#printAlert').addClass('alert-' + kind);
+  $('#printAlert').text(message);
+  if(duration > 0){
+    $('#printAlert').delay(duration).queue(function(next){
+      $(this).hide();
+      next();
+   });
+  }
+}
