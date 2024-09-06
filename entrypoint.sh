ccd0bccd54031d7acf3269365d09932c68c5969b
Guillaume BITON <guillaume@gbweb.fr>
2024-09-06 10:09:35 +0200

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 source /home/memorybox/venv/bin/activate
 
 if [ "$RUN_MODE" = "prod" ]; then
-  #gunicorn -w $GUNICORN_WORKERS memorybox.wsgi:app --bind 0.0.0.0:$GUNICORN_PORT --log-level $LOG_LEVEL
   gunicorn \
     -w $GUNICORN_WORKERS \
     -k eventlet   \
